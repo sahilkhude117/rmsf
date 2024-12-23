c0bdddf79bb3643195f2c7178c41bf8ef00da31c
sahilkhude117 <sahilkhude11@gmail.com>
2024-12-23 11:41:42 +0530

    migrated prisma

---
+++ prisma/migrations/20241223055914_update_user/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Added the required column `address` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `city` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `postalCode` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `state` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "address" TEXT NOT NULL,
+ADD COLUMN     "city" TEXT NOT NULL,
+ADD COLUMN     "postalCode" INTEGER NOT NULL,
+ADD COLUMN     "state" TEXT NOT NULL;
